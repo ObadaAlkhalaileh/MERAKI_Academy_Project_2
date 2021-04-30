b98ac75ed993e3ff8b6f7c8bfb3b70cebdbe2f34
Obada Alkhalaileh <alkhalaileh.obada@gmail.com>
2021-04-30 22:28:13 +0400

    styling status window

--- home-js.js
+++ home-js.js
@@ -1,34 +1,40 @@
 //tabs show and hide
 $(`.o1`).on("click", () => {
-    $(`.w1`).fadeIn()
-    $(".w2,.w3,.w4,.w5").css("display", "none")
-})
+    $(`.w1`).fadeIn();
+    $(".w2,.w3,.w4,.w5").css("display", "none");
+    $(".track").css("display", "none");
+
+});
 $(`.o2`).on("click", () => {
-    $(`.w2`).fadeIn()
-    $(".w1,.w3,.w4,.w5").css("display", "none")
-})
+    $(`.w2`).fadeIn();
+    $(".w1,.w3,.w4,.w5").css("display", "none");
+
+});
 $(`.o3`).on("click", () => {
-    $(`.w3`).fadeIn()
-    $(".w1,.w2,.w4,.w5").css("display", "none")
-})
+    $(`.w3`).fadeIn();
+    $(".w1,.w2,.w4,.w5").css("display", "none");
+});
 $(`.o4`).on("click", () => {
-    $(`.w4`).fadeIn()
-    $(".w1,.w2,.w3,.w5").css("display", "none")
-})
+    $(`.w4`).fadeIn();
+    $(".w1,.w2,.w3,.w5").css("display", "none");
+});
 $(`.fa-plus-circle`).on("click", () => {
-    $(`.w5`).fadeIn()
-    $(".w1,.w2,.w3,.w4").css("display", "none")
-    $(`.add-action`).fadeOut()
-})
+    $(`.w5`).fadeIn();
+    $(".w1,.w2,.w3,.w4").css("display", "none");
+    $(`.add-action`).fadeOut();
+    $(".track").css("transform", "translateY(-250px)");
+});
 
 //light theme
 $(`.dropdown-item1`).on("click", () => {
-    $(`body`).css("background-color", "white")
+    $(`body`).css("background-color", "whitesmoke")
     $(`.top`).css("background-color", "#bfc7d9")
     $(`.fa-plus-circle`).css("background-color", "transparent")
     $(`.add-action`).css("background-image", 'url(chat-bubble-flipped.png)')
     $(`input`).css('background-color', 'white')
     $(`.track`).css('color', 'rgba(81, 117, 129, 0.65)')
+    $(`.status`).css('background-color', 'whitesmoke')
+
 })
 
 //dark theme
@@ -39,6 +45,7 @@ $(`.dropdown-item2`).on("click", () => {
     $(`.add-action`).css("background-image", 'url(dark.png)')
     $(`input`).css('background-color', 'rgba(187, 187, 187,1)')
     $(`.track`).css('color', 'rgba(191, 199, 217, .5)')
+    $(`.status`).css('background-color', 'rgba(187, 187, 187,.5)')
 })
 
 //functionality
