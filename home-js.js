7108fca1978a33782f31f30ac4971a4e5f6df7b7
Obada Alkhalaileh <alkhalaileh.obada@gmail.com>
2021-04-30 17:11:23 +0400

    finished theme functionality

--- home-js.js
+++ home-js.js
@@ -1,7 +1,7 @@
-const showAloneO1 = () => {
+$(`.o1`).on("click", () => {
     $(`.w1`).fadeIn()
     $(".w2,.w3,.w4").css("display", "none")
-}
+})
 const showAloneO2 = () => {
     $(`.w2`).fadeIn()
     $(".w1,.w3,.w4").css("display", "none")
@@ -22,9 +22,13 @@ $(`.dropdown-item1`).on("click", () => {
     $(`body`).css("background-color", "white")
     $(`.top`).css("background-color", "#bfc7d9")
     $(`.fa-plus-circle`).css("background-color", "transparent")
+    $(`.add-action`).css("background-image", 'url(chat-bubble-flipped.png)')
+    $(`#balance`).css('background-color', 'white')
 })
 $(`.dropdown-item2`).on("click", () => {
     $(`body`).css("background-color", "#626673")
     $(`.top`).css("background-color", "#517581")
     $(`.fa-plus-circle`).css("background-color", "#999")
+    $(`.add-action`).css("background-image", 'url(dark.png)')
+    $(`#balance`).css('background-color', '#999')
 })
