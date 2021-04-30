75ce4e95de02a04108306e3873135f4e39c07c93
Obada Alkhalaileh <alkhalaileh.obada@gmail.com>
2021-04-30 17:55:18 +0400

    getting started with creating objects

--- home-js.js
+++ home-js.js
@@ -20,18 +20,30 @@ $(`.fa-plus-circle`).on("click", () => {
     $(`.add-action`).fadeOut()
 })
 
-
+//light theme
 $(`.dropdown-item1`).on("click", () => {
     $(`body`).css("background-color", "white")
     $(`.top`).css("background-color", "#bfc7d9")
     $(`.fa-plus-circle`).css("background-color", "transparent")
     $(`.add-action`).css("background-image", 'url(chat-bubble-flipped.png)')
     $(`#balance`).css('background-color', 'white')
+    $(`.track`).css('color', 'rgba(81, 117, 129, 0.65)')
 })
+
+//dark theme
 $(`.dropdown-item2`).on("click", () => {
     $(`body`).css("background-color", "#626673")
     $(`.top`).css("background-color", "#517581")
     $(`.fa-plus-circle`).css("background-color", "#999")
     $(`.add-action`).css("background-image", 'url(dark.png)')
-    $(`#balance`).css('background-color', '#999')
+    $(`#balance`).css('background-color', 'rgba(153, 153, 153, 1)')
+    $(`.track`).css('color', 'rgba(191, 199, 217, .5)')
 })
+const payments = []
+const addPayment = (description, value, date) => {
+    let newPayment = {}
+    newPayment.description = description
+    newPayment.value = value
+    newPayment.date = date
+    return payments.push(newPayment)
+}
